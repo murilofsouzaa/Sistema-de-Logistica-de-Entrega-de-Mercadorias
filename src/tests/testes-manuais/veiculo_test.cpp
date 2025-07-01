42bf7c4e654129e88941883d4641f6ad274b5109
Murilo Freitas de Souza <onemurilo@gmail.com>
2025-07-01 14:18:23 -0300

    implementacao de teste manual da classe Veiculo

---
+++ src/tests/testes-manuais/veiculo_test.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "../../Veiculo/include/Veiculo.hpp"
+
+int main() {
+    std::cout << "Cadastrando veiculo...\n";
+    cadastrarVeiculo("ABC1234", "Fiorino", "disponivel", "Centro");
+    cadastrarVeiculo("XYZ5678", "Kombi", "disponivel", "Bairro A");
+    listarVeiculos();
+
+    std::cout << "\nTentando cadastrar veiculo com placa repetida...\n";
+    cadastrarVeiculo("ABC1234", "Uno", "disponivel", "Centro");
+
+    std::cout << "\nAtualizando veiculo...\n";
+    atualizarVeiculo("XYZ5678", "Kombi Nova", "ocupado", "Bairro B");
+    listarVeiculos();
+
+    std::cout << "\nExcluindo veiculo...\n";
+    excluirVeiculo("XYZ5678");
+    listarVeiculos();
+
+    std::cout << "\nTentando excluir veiculo inexistente...\n";
+    excluirVeiculo("ZZZ0000");
+
+    std::cout << "\nTestes manuais de Veiculo finalizados!\n";
+    return 0;
+}
