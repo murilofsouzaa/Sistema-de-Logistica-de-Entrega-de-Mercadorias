b91587463042ac55719aee6d1078d11e057d234a
Murilo Freitas de Souza <onemurilo@gmail.com>
2025-07-02 16:17:10 -0300

    Inserção de dados mockados para teste no main

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,19 @@
 #include "Local.hpp"      
 #include "Pedido.hpp"
 
+void inserirDadosTeste() {
+    
+    cadastrarLocal("Centro", 0.0f, 0.0f);
+    cadastrarLocal("Bairro A", 10.0f, 0.0f);
+    cadastrarLocal("Bairro B", 0.0f, 10.0f);
+
+    cadastrarVeiculo("ABC1234", "Fiorino", "disponivel", "Centro");
+    cadastrarVeiculo("XYZ5678", "Kombi", "disponivel", "Bairro A");
+
+    cadastrarPedido(1, "Centro", "Bairro A", 5.0f);
+    cadastrarPedido(2, "Bairro A", "Bairro B", 8.0f);
+    cadastrarPedido(3, "Centro", "Bairro B", 2.5f);
+}
 
 using namespace std;
 
@@ -12,6 +25,9 @@ int main() {
     std::setlocale(LC_ALL, "");
 
     int opcao;
+
+    // Inserir dados de teste automaticamente ao iniciar
+    inserirDadosTeste();
 
     do {
         cout << "\n===== MENU PRINCIPAL =====\n";
@@ -50,3 +66,7 @@ int main() {
 
     return 0;
 }
+
+//Para compilar: "g++ src/main.cpp src/Local/Local.cpp src/Veiculo/Veiculo.cpp src/Pedido/Pedido.cpp src/Rota/Rota.cpp src/Menu/Menu.cpp src/Backup/backupDados.cpp src/Backup/recuperarDados.cpp -Iinclude -o src/output/main.exe"
+
+// Para executar: "src/output/main.exe"
